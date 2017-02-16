7233e28965093489cfd5ae0bfb7c6dbb345cbd53
justine <justine.siccion@yahoo.com>
2017-02-16 22:38:40 +0800

    eto na

--- Database/clinic.sql
+++ Database/clinic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2017 at 09:16 AM
+-- Generation Time: Feb 16, 2017 at 03:37 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
