bd7216e4594d89be6cdd0fb1dfdcdd07483eb357
dkromblon <romblondaveson@gmail.com>
2017-03-30 23:25:21 +0800

    Added new icon

--- Application/Clinic/core/components/current_qty.php
+++ Application/Clinic/core/components/current_qty.php
@@ -2,4 +2,4 @@
 
 //Eto yung nag priprint sa extra operations
 
-echo "&nbsp; <a href='current.php?".$pkey_string."'><img src=\"/" . BASE_DIRECTORY . "/images/" . $_SESSION['icon_set'] . "/view.png\" alt=\"View\" title=\"View\"></a>";
+echo "&nbsp; <a href='current.php?".$pkey_string."'><img src=\"/" . BASE_DIRECTORY . "/images/" . $_SESSION['icon_set'] . "/medicine.png\" alt=\"View Quantity\" title=\"View Quantity\"></a>";
--- Application/Clinic/modules/clinic/medicine/current_qty.php
+++ Application/Clinic/modules/clinic/medicine/current_qty.php
@@ -1,2 +1,2 @@
 <?php
-echo "&nbsp; <a href='current.php?".$pkey_string."'><img src=\"/" . BASE_DIRECTORY . "/images/" . $_SESSION['icon_set'] . "/view.png\" alt=\"View\" title=\"View\"></a>";
+echo "&nbsp; <a href='current.php?".$pkey_string."'><img src=\"/" . BASE_DIRECTORY . "/images/" . $_SESSION['icon_set'] . "/delete.png\" alt=\"View\" title=\"View\"></a>";
