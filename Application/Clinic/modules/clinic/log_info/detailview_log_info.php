6080cbc25f69192a1c770d7ccd143d97a739daea
justine <justine.siccion@yahoo.com>
2017-04-06 01:15:25 +0800

    Updated

--- Application/Clinic/modules/clinic/log_info/detailview_log_info.php
+++ Application/Clinic/modules/clinic/log_info/detailview_log_info.php
@@ -33,7 +33,6 @@ if(isset($patient_type) && $patient_type == 'Employee')
 {
     //Show only Employee ID textbox
     $html->fields['student_id']['control_type'] = 'hidden';
-	$patient_type = 'Employee';
 }
 else
 {
--- Application/Clinic/modules/clinic/required_appointment/edit_required_appointment.php
+++ Application/Clinic/modules/clinic/required_appointment/edit_required_appointment.php
@@ -10,7 +10,6 @@ init_var($filter_used);
 init_var($page_from);
 init_var($filter_sort_asc);
 init_var($filter_sort_desc);
-init_var($student_name);
 
 if(isset($_GET['required_appointment_id']))
 {
@@ -68,10 +67,9 @@ require 'subclasses/required_appointment_html.php';
 $html = new required_appointment_html;
 $html->draw_header('Edit %%', $message, $message_type);
 $html->draw_listview_referrer_info($filter_field_used, $filter_used, $page_from, $filter_sort_asc, $filter_sort_desc);
-
-
-$html->fields['student_id']['companion'] = '<input type="text" name="student_name" placeholder="Patient name" value="'.$student_name.'" disabled>';
-
+init_var($student_name);
+$html->fields['student_id']['companion'] = '<input type="text" name="student_name" placeholder="patient name" value="'.$student_name.'" disabled>';
+$html->draw_hidden('required_appointment_id');
 
 $html->draw_controls('edit');
 
